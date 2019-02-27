9eef71eb2533cc7a301edc2e7e3da9a5b646b51a
yooyongsuk <bencrow@naver.com>
2019-02-27 21:38:18 +0900

    test11

--- MainActivity.java
+++ MainActivity.java
@@ -153,6 +153,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, ViewpagerTest.class);
                 startActivity(intent);
+
+
+
             }
         });
 
