2cdcb24dc7fd781d9920cac7cb6a4565465c768f
yooyongsuk <bencrow@naver.com>
2019-02-27 21:48:01 +0900

    test11

--- MainActivity.java
+++ MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -155,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
 
-
+                System.out.println("HomeActivity onResume");
             }
         });
 
